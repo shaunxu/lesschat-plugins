0d505215e6c46a9fa7abf68998539978e285ec7f
Shaun Xu <jfarrio@gmail.com>
2015-07-20 16:40:50 +0800

    dig into each events

--- msbuild-lesschat/msbuild-lesschat/msbuild-lesschat/LesschatLogger.cs
+++ msbuild-lesschat/msbuild-lesschat/msbuild-lesschat/LesschatLogger.cs
@@ -52,28 +52,64 @@ namespace msbuild_lesschat
             }
             _client = new LesschatClient(webhookUri);
 
+            eventSource.BuildStarted += (sender, e) =>
+             {
+                 _sb.AppendLine(GenerateMessage(string.Empty, e, "BuildStarted"));
+                 _indent++;
+             };
+
+            eventSource.BuildFinished += (sender, e) =>
+            {
+                _indent--;
+                _sb.AppendLine(GenerateMessage(string.Empty, e, "BuildFinished"));
+            };
+
             eventSource.ProjectStarted += (sender, e) =>
               {
-                  _sb.AppendLine(GenerateMessage(string.Empty, e));
+                  _sb.AppendLine(GenerateMessage(string.Empty, e, "ProjectStarted"));
                   _indent++;
               };
 
             eventSource.ProjectFinished += (sender, e) =>
               {
                   _indent--;
-                  _sb.AppendLine(GenerateMessage(string.Empty,  e));
+                  _sb.AppendLine(GenerateMessage(string.Empty, e, "ProjectFinished"));
               };
+
+            eventSource.TaskStarted += (sender, e) =>
+              {
+                  _sb.AppendLine(GenerateMessage(string.Empty, e, "TaskStarted"));
+                  _indent++;
+              };
+
+            eventSource.TaskFinished += (sender, e) =>
+            {
+                _indent--;
+                _sb.AppendLine(GenerateMessage(string.Empty, e, "TaskFinished"));
+            };
+
+            eventSource.TargetStarted += (sender, e) =>
+            {
+                _sb.AppendLine(GenerateMessage(string.Empty, e, "TargetStarted"));
+                _indent++;
+            };
+
+            eventSource.TargetFinished += (sender, e) =>
+            {
+                _indent--;
+                _sb.AppendLine(GenerateMessage(string.Empty, e, "TargetFinished"));
+            };
 
             eventSource.ErrorRaised += (sender, e) =>
               {
                   var line = string.Format(": Error {0}({1},{2}): ", e.File, e.LineNumber, e.ColumnNumber);
-                  _sb.AppendLine(GenerateMessage(line, e));
+                  _sb.AppendLine(GenerateMessage(line, e, "ErrorRaised"));
               };
 
             eventSource.WarningRaised += (sender, e) =>
             {
                 var line = string.Format(": Warning {0}({1},{2}): ", e.File, e.LineNumber, e.ColumnNumber);
-                _sb.AppendLine(GenerateMessage(line, e));
+                _sb.AppendLine(GenerateMessage(line, e, "WarningRaised"));
             };
 
             eventSource.MessageRaised += (sender, e) =>
@@ -82,12 +118,12 @@ namespace msbuild_lesschat
                     || (e.Importance == MessageImportance.Normal && IsVerbosityAtLeast(LoggerVerbosity.Normal))
                     || (e.Importance == MessageImportance.Low && IsVerbosityAtLeast(LoggerVerbosity.Detailed)))
                   {
-                      _sb.AppendLine(GenerateMessage(string.Empty, e));
+                      _sb.AppendLine(GenerateMessage(string.Empty, e, "MessageRaised"));
                   }
               };
         }
 
-        private string GenerateMessage(string line, BuildEventArgs e)
+        private string GenerateMessage(string line, BuildEventArgs e, string category)
         {
             var title = string.Compare(e.SenderName, "MSBuild", true) == 0 ? line : string.Format("{0}: {1}", e.SenderName, line);
             var result = string.Empty;
@@ -95,6 +131,7 @@ namespace msbuild_lesschat
             {
                 result += "    ";
             }
+            result += category + " > ";
             result += title;
             result += e.Message;
             return result;
@@ -102,8 +139,10 @@ namespace msbuild_lesschat
 
         private void OutputMessages(string message)
         {
-            var response = _client.SendAsync(message).Result;
-            Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+            Console.WriteLine(message);
+
+            //var response = _client.SendAsync(message).Result;
+            //Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
         }
 
         public override void Shutdown()
